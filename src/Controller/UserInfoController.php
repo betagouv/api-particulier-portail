86d53157c5bf824250ddb56f0e388854625f979d
Stanislas Bernard <bernardstanislas@gmail.com>
2020-04-03 17:32:29 +0200

    :sparkles: UserInfo route

---
+++ src/Controller/UserInfoController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Symfony\Component\Serializer\SerializerInterface;
+
+class UserInfoController extends AbstractController
+{
+    /**
+     * @var NormalizerInterface
+     */
+    private $normalizer;
+
+    public function __construct(NormalizerInterface $normalizer)
+    {
+        $this->normalizer = $normalizer;
+    }
+
+    /**
+     * @Route("/oauth/userinfo")
+     */
+    public function userInfoAction()
+    {
+        $serializedUser = $this->normalizer->normalize($this->getUser());
+        return new JsonResponse($serializedUser);
+    }
+}
