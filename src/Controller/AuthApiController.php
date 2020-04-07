398422ecbf3e833e747d6f92ac97e68c24bd3887
Stanislas Bernard <bernardstanislas@gmail.com>
2020-04-07 18:13:46 +0200

    :truck: Rename ApiAuth to AuthApi

--- src/Controller/AuthApiController.php
+++ src/Controller/AuthApiController.php
@@ -7,28 +7,28 @@ use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ApiAuthController
+class AuthApiController
 {
     /**
-     * @Route("/connect/api-auth", name="connect_api-auth_start")
+     * @Route("/connect/auth-api", name="connect_auth-api_start")
      */
     public function connectAction(ClientRegistry $clientRegistry)
     {
-        return $clientRegistry->getClient('api-auth')->redirect([], []);
+        return $clientRegistry->getClient('auth-api')->redirect([], []);
     }
 
     /**
-     * @Route("/connect/api-auth/check", name="connect_api-auth_check")
+     * @Route("/connect/auth-api/check", name="connect_auth-api_check")
      */
     public function connectCheckAction(ClientRegistry $clientRegistry)
     {
-        $client = $clientRegistry->getClient('api-auth');
+        $client = $clientRegistry->getClient('auth-api');
 
         try {
             $user = $client->fetchUser();
             return new JsonResponse($user->toArray());
         } catch (IdentityProviderException $e) {
-            var_dump($e->getMessage());
+            throw $e;
         }
     }
 }
--- src/EventSubscriber/OAuthAuthorizationSubscriber.php
+++ src/EventSubscriber/OAuthAuthorizationSubscriber.php
@@ -3,11 +3,22 @@
 namespace App\EventSubscriber;
 
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Security\Core\Security;
 use Trikoder\Bundle\OAuth2Bundle\Event\AuthorizationRequestResolveEvent;
 use Trikoder\Bundle\OAuth2Bundle\OAuth2Events;
 
 class OAuthAuthorizationSubscriber implements EventSubscriberInterface
 {
+    /**
+     * @var Security
+     */
+    private $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
     public static function getSubscribedEvents()
     {
         return [
@@ -17,6 +28,9 @@ class OAuthAuthorizationSubscriber implements EventSubscriberInterface
 
     public function onAuthorizationRequestResolve(AuthorizationRequestResolveEvent $event)
     {
-        $event->resolveAuthorization(AuthorizationRequestResolveEvent::AUTHORIZATION_APPROVED);
+        $user = $this->security->getUser();
+        if ($user && in_array("ROLE_USER", $user->getRoles())) {
+            $event->resolveAuthorization(AuthorizationRequestResolveEvent::AUTHORIZATION_APPROVED);
+        }
     }
 }
