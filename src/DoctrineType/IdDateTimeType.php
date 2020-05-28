be81156dcd3df0e941c683ce9486a47e632da6aa
Stanislas Bernard <bernardstanislas@gmail.com>
2020-05-28 17:27:37 +0200

    :card_file_box: Set anayltics precsion to microsecond

--- src/DoctrineType/IdDateTimeType.php
+++ src/DoctrineType/IdDateTimeType.php
@@ -3,20 +3,53 @@
 namespace App\DoctrineType;
 
 use App\Type\PrintableDateTimeImmutable;
+use DateTimeImmutable;
 use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\ConversionException;
 use Doctrine\DBAL\Types\DateTimeTzImmutableType;
 
+// Inspired by https://github.com/doctrine/dbal/issues/2873#issuecomment-535452749
 class IdDateTimeType extends DateTimeTzImmutableType
 {
+    public function getSQLDeclaration(array $fieldDeclaration, AbstractPlatform $platform)
+    {
+        return 'DATETIME(6) WITH TIME ZONE';
+    }
+
     public function convertToPHPValue($value, AbstractPlatform $platform)
     {
-        $dateTime = parent::convertToPHPValue($value, $platform);
+        if ($value === null || $value instanceof DateTimeImmutable) {
+            return $value;
+        }
+
+        $dateTime = DateTimeImmutable::createFromFormat('Y-m-d H:i:s.u', $value);
 
         if (!$dateTime) {
-            return $dateTime;
+            throw ConversionException::conversionFailedFormat(
+                $value,
+                $this->getName(),
+                $platform->getDateTimeTzFormatString()
+            );
         }
 
         return new PrintableDateTimeImmutable('@' . $dateTime->format('U'));
+    }
+
+    public function convertToDatabaseValue($value, AbstractPlatform $platform)
+    {
+        if (null === $value) {
+            return $value;
+        }
+
+        if ($value instanceof DateTimeImmutable) {
+            return $value->format('Y-m-d H:i:s.u');
+        }
+
+        throw ConversionException::conversionFailedInvalidType(
+            $value,
+            $this->getName(),
+            ['null', 'DateTime']
+        );
     }
 
     public function getName()
