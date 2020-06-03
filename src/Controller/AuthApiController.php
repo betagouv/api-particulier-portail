2b524b64f324508941c50a0bcaf33df7694168e9
Stanislas Bernard <bernardstanislas@gmail.com>
2020-06-03 16:42:09 +0200

    :sparkles: Add logout

--- src/Controller/AuthApiController.php
+++ src/Controller/AuthApiController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Exception;
 use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
 use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -30,5 +31,14 @@ class AuthApiController
         } catch (IdentityProviderException $e) {
             throw $e;
         }
+    }
+
+    /**
+     * @Route("/connect/auth-api/logout", name="connect_auth-api_logout")
+     */
+    public function logoutAction()
+    {
+        // controller can be blank: it will never be executed!
+        throw new Exception('Don\'t forget to activate logout in security.yaml');
     }
 }
