3f66c86c4f5c75745827712bf401de7d38e6868f
Stanislas Bernard <bernardstanislas@gmail.com>
2020-06-12 16:08:44 +0200

    :sparkles: Add API model along with migrations

---
+++ app/db.py
@@ -0,0 +1,17 @@
+import uuid
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from sqlalchemy.dialects.postgresql import UUID
+
+from app import app
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+class Api(db.Model):
+    id = db.Column(UUID(as_uuid=True), primary_key=True,
+                   default=uuid.uuid4, unique=True, nullable=False)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    backend = db.Column(db.String(80), unique=True, nullable=False)
+    path = db.Column(db.String(80), unique=True, nullable=False)
