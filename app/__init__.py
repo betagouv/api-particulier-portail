41a817921f9b2496fc4c2df1a77d120378b84919
Stanislas Bernard <bernardstanislas@gmail.com>
2020-06-12 16:08:44 +0200

    :sparkles: Add API model along with migrations

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,9 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 from app import routes
+from app import db
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,9 @@
 from app import app
+from app.db import Api
 
 
 @app.route('/')
 @app.route('/index')
 def index():
+    apis = Api.query.all()
     return "Hello, croute!"
