39ab01469286d199b0f464189f81778590e66805
Stanislas Bernard <bernardstanislas@gmail.com>
2020-06-13 11:17:22 +0200

    :sparkles: Handle trailing path

--- app/routes.py
+++ app/routes.py
@@ -4,23 +4,40 @@ from flask import request, Response
 import requests
 
 
-def proxy(**kwargs):
+def proxy(url=None, **kwargs):
     api = kwargs['api']
-    r = requests.request(request.method, api.backend,
-                         params=request.args)
+    if url:
+        url = "{}/{}".format(api.backend, url)
+    else:
+        url = api.backend
+
+    r = requests.request(request.method, url, params=request.args)
+
     headers = dict(r.raw.headers)
     if 'Transfer-Encoding' in headers:
         del headers['Transfer-Encoding']
     if 'Content-Encoding' in headers:
         del headers['Content-Encoding']
-    print(headers)
 
     out = Response(r.text, headers=headers)
     out.status_code = r.status_code
     return out
 
 
-for api in Api.query.all():
-    app.add_url_rule("/{}".format(api.path), api.name, proxy, defaults={
-        'api': api
-    })
+def build_routes():
+    for api in Api.query.all():
+        app.add_url_rule("/{}/<path:url>".format(api.path), api.name, proxy, defaults={
+            'api': api
+        })
+        app.add_url_rule("/{}/".format(api.path), "{} - Root with slash".format(api.name), proxy, defaults={
+            'api': api
+        })
+        app.add_url_rule("/{}".format(api.path), "{} - Root".format(api.name), proxy, defaults={
+            'api': api
+        })
+
+
+try:
+    build_routes()
+except Exception as err:
+    print(err)
