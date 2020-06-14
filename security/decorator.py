32b976041c6066fad350f49f38fe39e564fa5058
Stanislas Bernard <bernardstanislas@gmail.com>
2020-06-14 13:15:36 +0200

    :sparkles: Check api key in db

--- security/decorator.py
+++ security/decorator.py
@@ -1,6 +1,7 @@
 from functools import wraps
 from flask import request, abort
 from security.hasher import hash_api_key
+from gateway.models import ApiKey
 
 
 def require_api_key(view_function):
@@ -8,12 +9,9 @@ def require_api_key(view_function):
     def decorated_view_function(*args, **kwargs):
         if request.headers.has_key("x-api-key"):
             hashed_api_key = hash_api_key(request.headers.get("x-api-key"))
-            if (
-                hashed_api_key
-                == "5f6c070b2f2126b39cb8c72cd05e99588c189a7ef689d7f7c5a0d1ed337068e525485469aa007f7202335774150474875a9ac28d5b6f433d103dcd4e25fc48e4"
-            ):
+            stored_api_key = ApiKey.query.filter_by(hashed_key=hashed_api_key).first()
+            if stored_api_key:
                 return view_function(*args, **kwargs)
-        else:
-            abort(401)
+        abort(401)
 
     return decorated_view_function
--- tests/test_api.py
+++ tests/test_api.py
@@ -6,13 +6,10 @@ def test_empty_db(client):
     assert response.status_code == 404
 
 
-def test_test_api(app, test_api, client):
+def test_test_api(app, test_api, client, test_api_key_value):
     build_routes()
-    print(app.url_map)
     response = client.get(
         "/{}/pokemon/ditto".format(test_api.path),
-        headers={
-            "X-Api-Key": "f25ba6ce8729ae6c3dd2493337a999de8d5c94b05dd5d775b306f18477621b29d9df626355b1bd011baf27749beb20a1"
-        },
+        headers={"X-Api-Key": test_api_key_value},
     )
     assert response.status_code == 200
