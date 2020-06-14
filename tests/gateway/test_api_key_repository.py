9191cad356d5d1213ea3c5d29a4c8d52d303fbc3
Stanislas Bernard <bernardstanislas@gmail.com>
2020-06-14 14:14:57 +0200

    :sparkles: Check active keys

---
+++ tests/gateway/test_api_key_repository.py
@@ -0,0 +1,13 @@
+from gateway.api_key_repository import get_active_by_hashed_key
+
+
+def test_missing_key():
+    assert get_active_by_hashed_key("yolo") is None
+
+
+def test_active_key(test_api_key):
+    assert get_active_by_hashed_key(test_api_key.hashed_key) is test_api_key
+
+
+def test_inactive_key(test_inactive_api_key):
+    assert get_active_by_hashed_key(test_inactive_api_key.hashed_key) is None
